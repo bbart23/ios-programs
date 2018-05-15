7820be0f9dc919ff47bea552e4b1976850e22f38
Brandon Bart <brandonbart@me.com>
2018-05-15 04:07:25 -0400

    Initial Commit

--- PI to the Nth Digit/ViewController.swift
+++ PI to the Nth Digit/ViewController.swift
@@ -1,8 +1,8 @@
 //
 //  ViewController.swift
-//  PI to the Nth Digit
+//  Exercises
 //
-//  Created by Brandon Bart on 5/15/18.
+//  Created by Brandon Bart on 5/14/18.
 //  Copyright © 2018 Bart. All rights reserved.
 //
 
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var piLabel: UILabel!
+    
+    @IBAction func stepper(_ sender: UIStepper) {
+        
+        label.text = String(Int(sender.value))
+        if (sender.value == 0){
+            piLabel.text = "3"
+        } else {
+        let digits = floor((Double.pi)*(pow(10.0,sender.value)))
+        piLabel.text = String((digits/pow(10.0,Double(sender.value))))
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
